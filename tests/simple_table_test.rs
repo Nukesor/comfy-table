c3190479f104a6781cd2f6f770f830b24b6eeb83
Arne Beer <contact@arne.beer>
2020-01-19 20:13:33 +0100

    Split tests

--- tests/simple_table_test.rs
+++ tests/simple_table_test.rs
@@ -1,5 +1,4 @@
 use comfy_table::prelude::*;
-use comfy_table::styling::presets::*;
 
 #[test]
 fn simple_table() {
@@ -30,75 +29,3 @@ fn simple_table() {
     assert_eq!("\n".to_string() + &table.to_string(), expected);
 }
 
-
-#[test]
-fn presets_work() {
-    let mut table = Table::new();
-    table.set_header(&vec!["Header1", "Header2", "Header3"]);
-    table.add_row(&vec![
-        "One One",
-        "One Two",
-        "One Three",
-    ]);
-    table.add_row(&vec![
-        "One One",
-        "One Two",
-        "One Three",
-    ]);
-
-    table.load_preset(ASCII_NO_BORDERS);
-    let expected = concat!(
-        " Header1 | Header2 | Header3   \n",
-        "===============================\n",
-        " One One | One Two | One Three \n",
-        "---------+---------+-----------\n",
-        " One One | One Two | One Three ",
-    );
-    assert_eq!(&table.to_string(), expected);
-
-
-    table.load_preset(ASCII_BORDERS_ONLY);
-    let expected = "
-+-------------------------------+
-| Header1   Header2   Header3   |
-+===============================+
-| One One   One Two   One Three |
-|                               |
-| One One   One Two   One Three |
-+-------------------------------+";
-    assert_eq!("\n".to_string() + &table.to_string(), expected);
-
-
-    table.load_preset(ASCII_HORIZONTAL_BORDERS_ONLY);
-    let expected = concat!(
-        "-------------------------------\n",
-        " Header1   Header2   Header3   \n",
-        "===============================\n",
-        " One One   One Two   One Three \n",
-        "-------------------------------\n",
-        " One One   One Two   One Three \n",
-        "-------------------------------",
-    );
-    assert_eq!(&table.to_string(), expected);
-
-    table.load_preset(UTF8_FULL);
-    let expected = "
-┌─────────┬─────────┬───────────┐
-│ Header1 ┆ Header2 ┆ Header3   │
-╞═════════╪═════════╪═══════════╡
-│ One One ┆ One Two ┆ One Three │
-├╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌┤
-│ One One ┆ One Two ┆ One Three │
-└─────────┴─────────┴───────────┘";
-    assert_eq!("\n".to_string() + &table.to_string(), expected);
-
-    table.load_preset(UTF8_BORDERS_ONLY);
-    let expected = "
-┌───────────────────────────────┐
-│ Header1   Header2   Header3   │
-╞═══════════════════════════════╡
-│ One One   One Two   One Three │
-│ One One   One Two   One Three │
-└───────────────────────────────┘";
-    assert_eq!("\n".to_string() + &table.to_string(), expected);
-}
