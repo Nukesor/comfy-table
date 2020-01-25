8acf07695ab0c38d339a0092a623ba449007a5e4
Arne Beer <contact@arne.beer>
2020-01-25 20:52:07 +0100

    More docs

--- src/style/mod.rs
+++ src/style/mod.rs
@@ -1,20 +1,22 @@
 /// This module contains some styling presets for tables.
-/// Each constant has an example for the respective preset.
+/// Every preset has an example. \
 pub mod presets;
 
-/// This module contains some constants, which can be used to modify certain parts of a preset.
+/// This module contains some constants, which can be used to modify certain parts of a preset.\
 /// For instance, the [UTF8_ROUND_CORNERS](modifiers::UTF8_ROUND_CORNERS) modifies all corners to be round UTF8 box corners.
-///
-/// All constants in this module consist of strings, with each character in the same position as the respective component in the [Component](table::Component) enum.
-/// Only non-whitespace characters are considered. You can use this, to write your own modifiers as well.
 pub mod modifiers;
 
 /// Everything about styling tables.
 pub mod table;
 
+/// Cell Alignment
 pub mod cell;
 
 pub mod column;
+
+pub use cell::CellAlignment;
+pub use table::{Component, ContentArrangement};
+pub use column::Constraint;
 
 /// Reexport for important crossterm enums
 pub use ::crossterm::style::Attribute;
