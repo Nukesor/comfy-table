35fb67b8fc07d446b831cefb0893ddab5e74d3fb
Arne Beer <contact@arne.beer>
2020-02-10 00:14:44 +0100

    Allow None in proptest constraints/alignments

--- tests/property_test.rs
+++ tests/property_test.rs
@@ -10,24 +10,26 @@ fn content_arrangement() -> impl Strategy<Value = ContentArrangement> {
   ]
 }
 
-fn cell_alignment() -> impl Strategy<Value = CellAlignment> {
+fn cell_alignment() -> impl Strategy<Value = Option<CellAlignment>> {
   prop_oneof![
-    Just(CellAlignment::Left),
-    Just(CellAlignment::Right),
-    Just(CellAlignment::Center),
+    Just(None),
+    Just(Some(CellAlignment::Left)),
+    Just(Some(CellAlignment::Right)),
+    Just(Some(CellAlignment::Center)),
   ]
 }
 
 
-fn column_constraint() -> impl Strategy<Value = ColumnConstraint> {
+fn column_constraint() -> impl Strategy<Value = Option<ColumnConstraint>> {
   prop_oneof![
-    Just(ColumnConstraint::ContentWidth),
-    any::<u16>().prop_map(ColumnConstraint::Width),
-    any::<u16>().prop_map(ColumnConstraint::MinWidth),
-    any::<u16>().prop_map(ColumnConstraint::MaxWidth),
-    (0u16..100u16).prop_map(ColumnConstraint::Percentage),
-    (0u16..100u16).prop_map(ColumnConstraint::MinPercentage),
-    (0u16..100u16).prop_map(ColumnConstraint::MaxPercentage),
+    Just(None),
+    Just(Some(ColumnConstraint::ContentWidth)),
+    any::<u16>().prop_map(|width| {Some(ColumnConstraint::Width(width))}),
+    any::<u16>().prop_map(|width| {Some(ColumnConstraint::MinWidth(width))}),
+    any::<u16>().prop_map(|width| {Some(ColumnConstraint::MaxWidth(width))}),
+    (0u16..100u16).prop_map(|percentage| {Some(ColumnConstraint::Percentage(percentage))}),
+    (0u16..100u16).prop_map(|percentage| {Some(ColumnConstraint::MinPercentage(percentage))}),
+    (0u16..100u16).prop_map(|percentage| {Some(ColumnConstraint::MaxPercentage(percentage))}),
   ]
 }
 
@@ -38,22 +40,31 @@ prop_compose! {
    }
 }
 
-fn columns_and_rows() -> impl Strategy<Value = (Vec<Vec<String>>, Vec<ColumnConstraint>, Vec<CellAlignment>)> {
+fn columns_and_rows() -> impl Strategy<Value = (
+    Vec<Vec<String>>,
+    Vec<Option<ColumnConstraint>>,
+    Vec<Option<CellAlignment>>,
+    Vec<Option<CellAlignment>>
+)> {
     dimensions().prop_flat_map(|(column_count, row_count)| {
         let mut rows = Vec::new();
-        let mut alignments = Vec::new();
+        let mut cell_alignments = Vec::new();
         for _i in 0..row_count {
+            // Create the max amount of possibly needed cell alignments
             for _j in 0..column_count {
-                alignments.push(cell_alignment());
+                cell_alignments.push(cell_alignment());
             }
+            // Add a strategy that creates random cell content with a length of 0 to column_count
             rows.push(::proptest::collection::vec(".*", 0..column_count as usize));
         }
         let mut constraints = Vec::new();
+        let mut column_alignments = Vec::new();
         for _i in 0..column_count {
             constraints.push(column_constraint());
+            column_alignments.push(cell_alignment());
         }
 
-        (rows, constraints, alignments)
+        (rows, constraints, cell_alignments, column_alignments)
     })
 }
 
@@ -62,23 +73,41 @@ prop_compose! {
     fn table()
         (arrangement in content_arrangement(),
         table_width in 0..500u16,
-        (rows, constraints, alignments) in columns_and_rows()) -> Table {
+        (rows, constraints, cell_alignments, column_alignments) in columns_and_rows()) -> Table {
         let mut table = Table::new();
 
-        let mut alignments = alignments.iter();
+        let mut cell_alignments = cell_alignments.iter();
         for row in rows.iter() {
             // Convert a vector of Strings to a vector of Cells and
             // set the content alignment for each cell
             let row: Vec<Cell> = row.iter().map(|content| {
-                Cell::new(content.clone())
-                    .set_alignment(*alignments.next().unwrap())
+                let mut cell = Cell::new(content.clone());
+                if let Some(alignment) = cell_alignments.next().unwrap() {
+                    cell = cell.set_alignment(*alignment);
+                }
+                cell
             }).collect();
+
             // Add the row to the table
             table.add_row(row);
         }
 
-        table.set_constraints(constraints)
-            .set_table_width(table_width)
+        for (column_index, column) in table.column_iter_mut().enumerate() {
+            // Add the alignment for all columns
+            let alignment = column_alignments.get(column_index).unwrap();
+            if let Some(alignment) = alignment {
+                column.set_cell_alignment(*alignment);
+            }
+
+            // Set constraints for all columns
+            let constraint = constraints.get(column_index).unwrap();
+            if let Some(constraint) = constraint {
+                column.set_constraint(*constraint);
+            }
+
+        }
+
+        table.set_table_width(table_width)
             .set_content_arrangement(arrangement)
             .load_preset(UTF8_FULL)
             .apply_modifier(UTF8_ROUND_CORNERS);
