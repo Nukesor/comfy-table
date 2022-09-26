c62aed251e7e4ed7904d4bea31c3889181aed455
Erich Spaker <erichspaker@gmail.com>
2022-09-26 09:29:07 -0400

    inner_style example to illustrate problem with current solution

--- examples/inner_style.rs
+++ examples/inner_style.rs
@@ -1,10 +1,12 @@
-use comfy_table::{Table, Row, Cell};
+use comfy_table::{Table, Row, Cell, ContentArrangement};
 use console::Style;
 
 fn main() {
     let mut table = Table::new();
     table.load_preset(comfy_table::presets::NOTHING);
-    
+    table.set_content_arrangement(ContentArrangement::Dynamic);
+    table.set_width(90);
+
     let mut row = Row::new();
     row.add_cell(Cell::new(
         "123 cell1"
@@ -17,10 +19,19 @@ fn main() {
 
     let mut row = Row::new();
     row.add_cell(Cell::new(
-        format!("cell{}", console::style("3").bold().red())
+        format!(r"cell sys-devices-pci0000:00-0000:00:010:07:00.1-usb2-2\x2d1-2\x2d1.3-2\x2d1.3:1.0-host2-target2:0:0-2:0:0:1-block-sdb{}", console::style(".device").bold().red())
     ));
     row.add_cell(Cell::new(
-        "cell4"
+        "cell4 asdfasfsad asdfasdf sad fas df asdf as df asdf    asdfasdfasdfasdfasdfasdfa dsfa sdf asdf asd f asdf as df sadf asd fas df "
+    ));
+    table.add_row(row);
+
+    let mut row = Row::new();
+    row.add_cell(Cell::new(
+        "cell5"
+    ));
+    row.add_cell(Cell::new(
+        "cell6"
     ));
     table.add_row(row);
 
