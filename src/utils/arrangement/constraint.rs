Nukesor/comfy-table

commit bee764d974bed2fb071cfa5bbb7248653cfdcf1a

change: Don't calculate intermediate column content widths

src/utils/arrangement/constraint.rs
@@ -13,14 +13,15 @@ use crate::{Column, Table};
 /// - The Column is supposed to be hidden.
 pub fn evaluate(
     table: &Table,
-    column: &Column,
-    infos: &mut DisplayInfos,
     table_width: Option<usize>,
     visible_columns: usize,
+    infos: &mut DisplayInfos,
+    column: &Column,
+    max_content_width: u16,
 ) {
     match &column.constraint {
         Some(ContentWidth) => {
-            let info = ColumnDisplayInfo::new(column, column.max_content_width);
+            let info = ColumnDisplayInfo::new(column, max_content_width);
             infos.insert(column.index, info);
         }
         Some(Absolute(width)) => {
@@ -34,7 +35,7 @@ pub fn evaluate(
             }
         }
         Some(Hidden) => {
-            let mut info = ColumnDisplayInfo::new(column, column.max_content_width);
+            let mut info = ColumnDisplayInfo::new(column, max_content_width);
             info.is_hidden = true;
             infos.insert(column.index, info);
         }
@@ -44,7 +45,7 @@ pub fn evaluate(
     if let Some(min_width) = min(table, &column.constraint, table_width, visible_columns) {
         // In case a min_width is specified, we may already fix the size of the column.
         // We do this, if we know that the content is smaller than the min size.
-        if column.max_width() <= min_width {
+        if max_content_width <= min_width {
             let width = absolute_width_with_padding(column, min_width);
             let info = ColumnDisplayInfo::new(column, width);
             infos.insert(column.index, info);

src/column.rs
@@ -38,7 +38,6 @@ pub struct Column {
     pub(crate) delimiter: Option<char>,
     /// Define the [CellAlignment] for all cells of this column
     pub(crate) cell_alignment: Option<CellAlignment>,
-    pub(crate) max_content_width: u16,
     pub(crate) constraint: Option<ColumnConstraint>,
 }
 
@@ -49,7 +48,6 @@ impl Column {
             padding: (1, 1),
             delimiter: None,
             constraint: None,
-            max_content_width: 0,
             cell_alignment: None,
         }
     }
@@ -78,18 +76,6 @@ impl Column {
         self.delimiter = Some(delimiter);
 
         self
-    }
-
-    /// Get the width in characters of the widest line in this column.\
-    /// This doesn't include padding yet!
-    pub fn max_content_width(&self) -> u16 {
-        self.max_content_width
-    }
-
-    /// Get the maximum possible width for this column.\
-    /// This means widest line in this column + padding
-    pub fn max_width(&self) -> u16 {
-        self.max_content_width + self.padding.0 + self.padding.1
     }
 
     /// Constraints allow to influence the auto-adjustment behavior of columns.\
@@ -132,7 +118,6 @@ mod tests {
     fn test_column() {
         let mut column = Column::new(0);
         column.set_padding((0, 0));
-        assert_eq!(column.max_content_width(), 0);
 
         column.set_constraint(ColumnConstraint::ContentWidth);
         assert_eq!(column.constraint(), Some(&ColumnConstraint::ContentWidth));

src/table.rs
@@ -109,7 +109,6 @@ impl Table {
     pub fn set_header<T: Into<Row>>(&mut self, row: T) -> &mut Self {
         let row = row.into();
         self.autogenerate_columns(&row);
-        self.adjust_max_column_widths(&row);
         self.header = Some(row);
 
         self
@@ -131,7 +130,6 @@ impl Table {
     pub fn add_row<T: Into<Row>>(&mut self, row: T) -> &mut Self {
         let mut row = row.into();
         self.autogenerate_columns(&row);
-        self.adjust_max_column_widths(&row);
         row.index = Some(self.rows.len());
         self.rows.push(row);
 
@@ -567,13 +565,33 @@ impl Table {
     }
 
     /// Return a vector representing the maximum amount of characters in any line of this column.\
-    /// This is mostly needed for internal testing and formatting, but can be interesting
-    /// if you want to see the widths of the longest lines for each column.
-    pub fn column_max_content_widths(&self) -> Vec<u16> {
-        self.columns
-            .iter()
-            .map(|column| column.max_content_width)
-            .collect()
+    /// This is needed for internal testing and formatting.
+    pub(crate) fn column_max_content_widths(&self) -> Vec<u16> {
+        fn set_max_content_widths(max_widths: &mut [u16], row: &Row) {
+            // Get the max width for each cell of the row
+            let row_max_widths = row.max_content_widths();
+            for (index, width) in row_max_widths.iter().enumerate() {
+                let width = (*width).try_into().unwrap_or(u16::MAX);
+
+                // Set a new max, if the current cell is the longest for that column.
+                let current_max = max_widths[index];
+                if current_max < width {
+                    max_widths[index] = width;
+                }
+            }
+        }
+        // The vector that'll contain the max widths per column.
+        let mut max_widths = vec![0; self.columns.len()];
+
+        if let Some(header) = &self.header {
+            set_max_content_widths(&mut max_widths, header);
+        }
+        // Iterate through all rows of the table.
+        for row in self.rows.iter() {
+            set_max_content_widths(&mut max_widths, row);
+        }
+
+        max_widths
     }
 
     pub(crate) fn style_or_default(&self, component: TableComponent) -> String {
@@ -592,19 +610,6 @@ impl Table {
         if row.cell_count() > self.columns.len() {
             for index in self.columns.len()..row.cell_count() {
                 self.columns.push(Column::new(index));
-            }
-        }
-    }
-
-    /// Update the max_content_width for all columns depending on the new row
-    fn adjust_max_column_widths(&mut self, row: &Row) {
-        let max_widths = row.max_content_widths();
-        for (index, width) in max_widths.iter().enumerate() {
-            let width = (*width).try_into().unwrap_or(u16::MAX);
-            // We expect this column to exist, since we autoenerate columns just before calling this function
-            let mut column = self.columns.get_mut(index).unwrap();
-            if column.max_content_width < width {
-                column.max_content_width = width;
             }
         }
     }

src/utils/arrangement/disabled.rs
@@ -6,13 +6,18 @@ use crate::Table;
 /// Dynamic arrangement is disabled.
 /// Apply all non-relative constraints, and set the width of all remaining columns to the
 /// respective max content width.
-pub fn arrange(table: &Table, infos: &mut DisplayInfos, visible_columns: usize) {
+pub fn arrange(
+    table: &Table,
+    infos: &mut DisplayInfos,
+    visible_columns: usize,
+    max_content_widths: &[u16],
+) {
     for column in table.columns.iter() {
         if infos.contains_key(&column.index) {
             continue;
         }
 
-        let mut width = column.max_content_width();
+        let mut width = max_content_widths[column.index];
 
         // Reduce the width, if a column has longer content than the specified MaxWidth constraint.
         if let Some(max_width) = constraint::max(table, &column.constraint, None, visible_columns) {

src/utils/arrangement/dynamic.rs
@@ -26,7 +26,12 @@ use crate::{Column, Table};
 ///
 /// 1. A user assigns more space to a few columns than there is on the terminal
 /// 2. A user provides more than 100% column width over a few columns.
-pub fn arrange(table: &Table, infos: &mut DisplayInfos, table_width: usize) {
+pub fn arrange(
+    table: &Table,
+    infos: &mut DisplayInfos,
+    table_width: usize,
+    max_content_widths: &[u16],
+) {
     let visible_columns = count_visible_columns(&table.columns);
 
     // Step 1
@@ -40,16 +45,21 @@ pub fn arrange(table: &Table, infos: &mut DisplayInfos, table_width: usize) {
     // Step 2-4.
     // Find all columns that require less space than the average.
     // Returns the remaining available width and the amount of remaining columns that need handling
-    let (mut remaining_width, mut remaining_columns) =
-        find_columns_less_than_average(table, infos, table_width, remaining_width, visible_columns);
+    let (mut remaining_width, mut remaining_columns) = find_columns_less_than_average(
+        table,
+        infos,
+        table_width,
+        remaining_width,
+        visible_columns,
+        max_content_widths,
+    );
 
     // Step 5.
     //
     // Iterate through all undecided columns and enforce LowerBoundary constraints, if they're
     // bigger than the current average space.
     if remaining_columns > 0 {
-        // TODO: Refactor once destructuring assignments are stable.
-        let (width, columns) = enforce_lower_boundary_constraints(
+        (remaining_width, remaining_columns) = enforce_lower_boundary_constraints(
             table,
             infos,
             table_width,
@@ -57,8 +67,6 @@ pub fn arrange(table: &Table, infos: &mut DisplayInfos, table_width: usize) {
             remaining_columns,
             visible_columns,
         );
-        remaining_width = width;
-        remaining_columns = columns;
     }
 
     #[cfg(feature = "debug")]
@@ -74,15 +82,13 @@ pub fn arrange(table: &Table, infos: &mut DisplayInfos, table_width: usize) {
     // We only do this if there are remaining columns.
     if remaining_columns > 0 {
         // This is where Step 5 happens.
-        let (width, columns) = optimize_space_after_split(
+        (remaining_width, remaining_columns) = optimize_space_after_split(
             table,
             &table.columns,
             infos,
             remaining_width,
             remaining_columns,
         );
-        remaining_width = width;
-        remaining_columns = columns;
     }
 
     #[cfg(feature = "debug")]
@@ -189,6 +195,7 @@ fn find_columns_less_than_average(
     table_width: usize,
     mut remaining_width: usize,
     visible_coulumns: usize,
+    max_content_widths: &[u16],
 ) -> (usize, usize) {
     let mut found_smaller = true;
     let mut remaining_columns = count_remaining_columns(visible_coulumns, infos);
@@ -213,8 +220,10 @@ fn find_columns_less_than_average(
                 continue;
             }
 
+            let max_column_width = max_content_widths[column.index];
+
             // The column has a MaxWidth Constraint.
-            // we can fix the column to this max_width and mark it as checked, if these
+            // we can fix the column to this max_width and mark it as checked if these
             // two conditions are met:
             // - The average remaining space is bigger then the MaxWidth constraint.
             // - The actual max content of the column is bigger than the MaxWidth constraint.
@@ -225,10 +234,13 @@ fn find_columns_less_than_average(
                 visible_coulumns,
             ) {
                 // Max/Min constraints always include padding!
-                let space_after_padding = average_space + usize::from(column.padding_width());
+                let average_space_with_padding =
+                    average_space + usize::from(column.padding_width());
 
+                let column_width_with_padding = max_column_width + column.padding_width();
                 // Check that both conditions mentioned above are met.
-                if usize::from(max_width) <= space_after_padding && column.max_width() >= max_width
+                if usize::from(max_width) <= average_space_with_padding
+                    && column_width_with_padding >= max_width
                 {
                     // Save the calculated info, this column has been handled.
                     let width = absolute_width_with_padding(column, max_width);
@@ -249,12 +261,12 @@ fn find_columns_less_than_average(
 
             // The column has a smaller max_content_width than the average space.
             // Fix the width to max_content_width and mark it as checked
-            if usize::from(column.max_content_width()) < average_space {
-                let info = ColumnDisplayInfo::new(column, column.max_content_width());
+            if usize::from(max_column_width) < average_space {
+                let info = ColumnDisplayInfo::new(column, max_column_width);
                 infos.insert(column.index, info);
 
                 // Continue with new recalculated width
-                remaining_width = remaining_width.saturating_sub(column.max_content_width.into());
+                remaining_width = remaining_width.saturating_sub(max_column_width.into());
                 remaining_columns -= 1;
                 if remaining_columns == 0 {
                     break;

src/utils/arrangement/mod.rs
@@ -17,10 +17,21 @@ pub(crate) fn arrange_content(table: &Table) -> Vec<ColumnDisplayInfo> {
     let table_width = table.width().map(usize::from);
     let mut infos = BTreeMap::new();
 
+    let max_content_widths = table.column_max_content_widths();
+
+    // Check if we can already resolve some constraints.
+    // This step also populates the ColumnDisplayInfo structs.
     let visible_columns = helper::count_visible_columns(&table.columns);
     for column in table.columns.iter() {
         if column.constraint.is_some() {
-            constraint::evaluate(table, column, &mut infos, table_width, visible_columns);
+            constraint::evaluate(
+                table,
+                table_width,
+                visible_columns,
+                &mut infos,
+                column,
+                max_content_widths[column.index],
+            );
         }
     }
     #[cfg(feature = "debug")]
@@ -31,14 +42,16 @@ pub(crate) fn arrange_content(table: &Table) -> Vec<ColumnDisplayInfo> {
     let table_width = if let Some(table_width) = table_width {
         table_width
     } else {
-        disabled::arrange(table, &mut infos, visible_columns);
+        disabled::arrange(table, &mut infos, visible_columns, &max_content_widths);
         return infos.into_iter().map(|(_, info)| info).collect();
     };
 
     match &table.arrangement {
-        ContentArrangement::Disabled => disabled::arrange(table, &mut infos, visible_columns),
+        ContentArrangement::Disabled => {
+            disabled::arrange(table, &mut infos, visible_columns, &max_content_widths)
+        }
         ContentArrangement::Dynamic | ContentArrangement::DynamicFullWidth => {
-            dynamic::arrange(table, &mut infos, table_width);
+            dynamic::arrange(table, &mut infos, table_width, &max_content_widths);
         }
     }
 

commit a524a268493f0d0d38300cca74aaff12e6fed322

fix: Wrong lower boundary constraint logic
This bug has been introduced in commit bee764d, when doing the
refactoring on how column content widths were calculated.

The old logic worked with the full width including paddings, the new
logic however only worked with the content width.

Due to this, columns were sometimes pinned to a width that was too small
for their actual content.

src/utils/arrangement/constraint.rs
@@ -45,7 +45,8 @@ pub fn evaluate(
     if let Some(min_width) = min(table, &column.constraint, table_width, visible_columns) {
         // In case a min_width is specified, we may already fix the size of the column.
         // We do this, if we know that the content is smaller than the min size.
-        if max_content_width <= min_width {
+        let max_width = max_content_width + column.padding_width();
+        if max_width <= min_width {
             let width = absolute_width_with_padding(column, min_width);
             let info = ColumnDisplayInfo::new(column, width);
             infos.insert(column.index, info);