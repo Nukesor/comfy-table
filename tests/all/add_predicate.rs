f625ad01ba92e7e687aa2c4b32b9d24b8ed26f8e
Techassi <git@techassi.dev>
2023-04-20 14:02:21 +0200

    Add 'add_rows_if' tests

--- tests/all/add_predicate.rs
+++ tests/all/add_predicate.rs
@@ -3,7 +3,7 @@ use pretty_assertions::assert_eq;
 use comfy_table::*;
 
 #[test]
-fn add_predicate_true() {
+fn add_predicate_single_true() {
     let mut table = Table::new();
     table
         .set_header(&vec!["Header1", "Header2", "Header3"])
@@ -36,7 +36,7 @@ fn add_predicate_true() {
 }
 
 #[test]
-fn add_predicate_false() {
+fn add_predicate_single_false() {
     let mut table = Table::new();
     table
         .set_header(&vec!["Header1", "Header2", "Header3"])
@@ -65,7 +65,7 @@ fn add_predicate_false() {
 }
 
 #[test]
-fn add_predicate_mixed() {
+fn add_predicate_single_mixed() {
     let mut table = Table::new();
     table
         .set_header(&vec!["Header1", "Header2", "Header3"])
@@ -85,6 +85,111 @@ fn add_predicate_mixed() {
                 "Now\nadd some\nmulti line stuff",
                 "This is awesome",
             ],
+        );
+
+    println!("{table}");
+    let expected = "
++----------------------+----------------------+------------------------+
+| Header1              | Header2              | Header3                |
++======================================================================+
+| This is a text       | This is another text | This is the third text |
+|----------------------+----------------------+------------------------|
+| This is another text | Now                  | This is awesome        |
+|                      | add some             |                        |
+|                      | multi line stuff     |                        |
++----------------------+----------------------+------------------------+";
+    assert_eq!("\n".to_string() + &table.to_string(), expected);
+}
+
+#[test]
+fn add_predicate_multi_true() {
+    let mut table = Table::new();
+    let rows = vec![
+        Row::from(&vec![
+            "This is a text",
+            "This is another text",
+            "This is the third text",
+        ]),
+        Row::from(&vec![
+            "This is another text",
+            "Now\nadd some\nmulti line stuff",
+            "This is awesome",
+        ]),
+    ];
+
+    table
+        .set_header(&vec!["Header1", "Header2", "Header3"])
+        .add_rows_if(|| true, rows);
+
+    println!("{table}");
+    let expected = "
++----------------------+----------------------+------------------------+
+| Header1              | Header2              | Header3                |
++======================================================================+
+| This is a text       | This is another text | This is the third text |
+|----------------------+----------------------+------------------------|
+| This is another text | Now                  | This is awesome        |
+|                      | add some             |                        |
+|                      | multi line stuff     |                        |
++----------------------+----------------------+------------------------+";
+    assert_eq!("\n".to_string() + &table.to_string(), expected);
+}
+
+#[test]
+fn add_predicate_multi_false() {
+    let mut table = Table::new();
+    table
+        .set_header(&vec!["Header1", "Header2", "Header3"])
+        .add_row(&vec![
+            "This is a text",
+            "This is another text",
+            "This is the third text",
+        ])
+        .add_rows_if(
+            || false,
+            vec![Row::from(&vec![
+                "This is another text",
+                "Now\nadd some\nmulti line stuff",
+                "This is awesome",
+            ])],
+        );
+
+    println!("{table}");
+    let expected = "
++----------------+----------------------+------------------------+
+| Header1        | Header2              | Header3                |
++================================================================+
+| This is a text | This is another text | This is the third text |
++----------------+----------------------+------------------------+";
+    assert_eq!("\n".to_string() + &table.to_string(), expected);
+}
+
+#[test]
+fn add_predicate_multi_mixed() {
+    let mut table = Table::new();
+    let rows = vec![
+        Row::from(&vec![
+            "This is a text",
+            "This is another text",
+            "This is the third text",
+        ]),
+        Row::from(&vec![
+            "This is another text",
+            "Now\nadd some\nmulti line stuff",
+            "This is awesome",
+        ]),
+    ];
+
+    table
+        .set_header(&vec!["Header1", "Header2", "Header3"])
+        .add_rows_if(|| true, rows)
+        .add_rows_if(
+            || false,
+            vec![Row::from(&vec![
+                "I won't get displayed",
+                "Me neither",
+                "Same here!",
+            ])],
         );
 
     println!("{table}");
