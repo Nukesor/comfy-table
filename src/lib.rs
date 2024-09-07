cca824a46e094900a15ad8f11c6740c6798ed3a8
Arne Beer <contact@arne.beer>
2024-09-07 22:40:10 +0200

    fix: Ignore false-positive clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 #![doc = include_str!("../README.md")]
 // The README code examples should be valid mini scripts to make them properly testable.
 #![allow(clippy::needless_doctest_main)]
+// Had a few false-positives on v1.81. Check lateron if they're still there.
+#![allow(clippy::manual_unwrap_or)]
 
 mod cell;
 mod column;
